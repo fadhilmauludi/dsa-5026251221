d2a4d02c244ae86a296b59a48d05e1dd827e60b3
Fadhil Mauludi <fadhilmauludi596@gmail.com>
2026-09-21 00:21:58 +0700

    Labwok 01 - Prelab

---
+++ bin/lw01/prelab/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(Main.class.getResourceAsStream("jobs.txt"));
+
+        List<PrintJob> jobs = new ArrayList<>();
+
+        for (int i=0; i<5; i++) {
+            String type = sc.next();
+            String id = sc.next();
+            int pages = sc.nextInt();
+
+            if (type.equals("MONO")) {
+                jobs.add(new MonoPrint(id, pages));
+            }
+            else if (type.equals("COLOUR")) {
+                jobs.add(new ColourPrint(id, pages));
+            }
+        }
+
+        for (int i=0; i<jobs.size(); i++) {
+            PrintJob job = jobs.get(i);
+            System.out.println(job.summary());
+        }
+    }   
+}
